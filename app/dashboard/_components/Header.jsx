1734abacd369f180e1280451c530d82257e7fd5a
Kirti Kamal <kirtikamal02@gmail.com>
2024-06-11 19:27:04 +0530

    created the ai-interview app

---
+++ app/dashboard/_components/Header.jsx
@@ -0,0 +1,33 @@
+"use client"
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react'
+
+function Header() {
+
+    const path = usePathname();
+    useEffect(() =>{
+
+    })
+
+
+
+  return (
+    <div className='flex p-4 items-center justify-between bg-secondary shadow-md'> 
+      <Image src={'./logo.svg'} width={160} height={100} alt='logo' />
+
+      <ul className='hidden md:flex gap-6'>
+        <li className={`hover: text-primary hover:font-bold transition cursor-pointer ${path === '/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
+        <li className={`hover: text-primary hover:font-bold transition cursor-pointer ${path === '/dashboard/questions' && 'text-primary font-bold'}`}>Questions</li>
+<Link href={'./upgrade'}>        <li className={`hover: text-primary hover:font-bold transition cursor-pointer ${path === '/dashboard/upgrade' && 'text-primary font-bold'}`}>Upgrade</li></Link>
+        <li className={`hover: text-primary hover:font-bold transition cursor-pointer ${path === '/dashboard/how' && 'text-primary font-bold'}`}>How it works ?</li>
+      </ul>
+
+      <UserButton/>
+    </div>
+  )
+}
+
+export default Header
